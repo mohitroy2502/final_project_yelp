a3ca9f0ec0fcd8631acdf685a8a620d1e38bab2d
Shivani Rana <srana8@hawk.iit.edu>
2019-11-23 20:07:03 -0600

    Update spark_hive_sub.scala

--- spark_hive_sub.scala
+++ spark_hive_sub.scala
@@ -12,3 +12,12 @@ Scala> business_review_rated.write.insertInto("yelp.business_review_rated ")
 //Top restaurants in number of listed categories (Restaurant Categories)
 Scala> val restaurant_categories= hiveCtx.jsonFile("hdfs://0.0.0.0:19000/rawdata/business/business.json").select("business_id","categories").repartition(1)
 Scala> restaurant_categories.write.insertInto("yelp.restaurant_categories ")                     
+
+//Average number of reviews per business star rating (Review Per Stars)
+Scala> val review_per_star= hiveCtx.jsonFile("hdfs://0.0.0.0:19000/rawdata/business/business.json").select("business_id","review_count","stars").repartition(1)
+Scala> review_per_star.write.insertInto("yelp.review_per_star")
+
+//Check the Saturday open and close times for few businesses (Saturday Open Close)
+Scala> val saturday_open_close= hiveCtx.jsonFile("hdfs://0.0.0.0:19000/rawdata/business/business.json").select("business_id","hours").repartition(1)
+Scala> Saturday_open_close.write.insertInto("yelp.Saturday_open_close")
+
